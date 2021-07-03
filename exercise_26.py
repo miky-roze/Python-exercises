368cf45a879066126e8222faed5f70439d449f92
miky_roze <michal.roek@gmail.com>
2021-07-03 20:55:56 +0200

    Adding example of executescript() method.

--- exercise_26.py
+++ exercise_26.py
@@ -1,5 +1,6 @@
 # Script creates SQL database (app.db) with table 'customers' with columns: customer_id, first_name, last_name, email.
-# Than adds two records, commits changes, reads the database and closes the connection
+# Than adds 5 records with execute() and executescript() methods.
+# Commits changes, reads the database and closes the connection.
 
 import sqlite3
 
@@ -13,14 +14,25 @@ commandsAppDB.execute("""CREATE TABLE IF NOT EXISTS customer (
     email TEXT)""")
 
 commandsAppDB.execute("""INSERT INTO customer (first_name, last_name, email) VALUES (
-    'John', 'Smith', 'john.smith@esmartdata.org')""")
+    'John', 'Smith', 'john.smith@mail.org')""")
 commandsAppDB.execute("""INSERT INTO customer (first_name, last_name, email) VALUES (
-    'Mike', 'Doe', 'mike.doe@esmartdata.org')""")
+    'Mike', 'Doe', 'mike.doe@mail.org')""")
+
+commandsAppDB.executescript("""
+INSERT INTO customer (first_name, last_name, email) VALUES (
+    'Matthew', 'Johnson', 'matthew.johnson@mail.org');
+
+INSERT INTO customer (first_name, last_name, email) VALUES (
+    'James', 'Durant', 'james.durant@mail.org');
+
+INSERT INTO customer (first_name, last_name, email) VALUES (
+    'Barrack', 'Obama', 'barrack.obama@mail.org');
+""")
 
 connectionAppDB.commit()
 
 customers = [customer for customer in commandsAppDB.execute("""SELECT * FROM customer""")]
 
-print(customers)
+# print(customers)
 
 connectionAppDB.close()
